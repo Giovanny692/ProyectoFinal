292ff41aa01534ac289d51fdbe7ac35063b33669
Giovanny692 <giovannymoreno492@gmail.com>
2023-05-27 15:53:07 -0500

    cambios en el formulario de entrada de visitantes

--- hoja.py
+++ hoja.py
@@ -5,7 +5,7 @@ class Hoja():
   def __init__(self,root = None):
     self.root = tk.Tk()
     self.root.title("Registro")
-    self.root.geometry("800x480")
+    self.root.geometry("800x600")
     self.frame=None
     self.frameresidentes=None
     self.crear_fmenu()
@@ -50,49 +50,76 @@ class Hoja():
      label_nombre = tk.Label(self.frame, text='Nombre: ')
      label_nombre.config(font= (('Arial', 12, 'bold')))
      label_nombre.grid(row = 0, column=0, padx= 10, pady=10)
-     #Duracion
-     label_duracion = tk.Label(self.frame, text='Id: ')
+     #ID
+     label_id = tk.Label(self.frame, text='ID:')
+     label_id.config(font= (('Arial', 12, 'bold')))
+     label_id.grid(row=1,column=0,padx=10,pady=10)
+     #Hora de entrada 
+     label_duracion = tk.Label(self.frame, text='Hora de entrada: ')
      label_duracion.config(font= (('Arial', 12, 'bold')))
-     label_duracion.grid(row = 1, column=0, padx= 10, pady=10)
-     #Genero
-     label_genero = tk.Label(self.frame, text='Hora: ')
-     label_genero.config(font= (('Arial', 12, 'bold')))
-     label_genero.grid(row = 2, column=0, padx= 10, pady=10)
-
+     label_duracion.grid(row = 2, column=0, padx= 10, pady=10)
+     #Hora de salida
+     label_id = tk.Label(self.frame, text='Hora de salida: ')
+     label_id.config(font= (('Arial', 12, 'bold')))
+     label_id.grid(row = 3, column=0, padx= 10, pady=10)
+     #Tiene vehiculo?
+     self.checkbox_vehiculovar = tk.BooleanVar(self.frame)
+     self.checkbox_vehiculo = tk.Checkbutton(self.frame,text='¿tiene vehiculo?',variable=self.checkbox_vehiculovar,command=self.habilitar_placa,state='disabled')
+     self.checkbox_vehiculo.config(font= (('Arial', 12, 'bold')))
+     self.checkbox_vehiculo.grid(row=4,column=0,padx=10,pady=10)
+     #Placa del vehiculo
+     label_placa=tk.Label(self.frame, text="Placa")
+     label_placa.config(font= (('Arial', 12, 'bold')))
+     label_placa.grid(row=5,column=0,padx=10,pady=10)
+     
      #Entrada de cada campo
      #Nombre
      self.entry_nombre = tk.Entry(self.frame)
      self.entry_nombre.config(width= 25, state= 'disabled' ,font= (('Arial', 12)))
      self.entry_nombre.grid(row = 0, column=1, padx= 10, pady=10)
-
-     #Duracion
-     self.entry_hora = tk.Entry(self.frame)
-     self.entry_hora.config(width= 25, state= 'disabled' ,font= (('Arial', 12)))
-     self.entry_hora.grid(row = 1, column=1, padx= 10, pady=10)
-     #Genero
-     self.entry_id = tk.Entry(self.frame)
+     #ID
+     self.entry_id= tk.Entry(self.frame)
      self.entry_id.config(width= 25, state= 'disabled' ,font= (('Arial', 12)))
-     self.entry_id.grid(row = 2, column=1, padx= 10, pady=10)
+     self.entry_id.grid(row = 1, column=1, padx= 10, pady=10)
+     #Hora entrada
+     self.entry_hora_entrada = tk.Entry(self.frame)
+     self.entry_hora_entrada.config(width= 25, state= 'disabled' ,font= (('Arial', 12)))
+     self.entry_hora_entrada.grid(row = 2, column=1, padx= 10, pady=10)
+     #Hora salida
+     self.entry_hora_salida = tk.Entry(self.frame)
+     self.entry_hora_salida.config(width= 25, state= 'disabled' ,font= (('Arial', 12)))
+     self.entry_hora_salida.grid(row = 3, column=1, padx= 10, pady=10)
+     #Placa vehiculo
+     self.entry_placa=tk.Entry(self.frame)
+     self.entry_placa.config(width= 25, state= 'disabled' ,font= (('Arial', 12)))
+     self.entry_placa.grid(row = 5, column=1, padx= 10, pady=10)
         
         
      #Botones
      boton_nuevo = tk.Button(self.frame, text = 'Nuevo',command=self.enable_espacios)
      boton_nuevo.config(width=20, font=('Arial', 12, 'bold'), fg='#DAD5D6', bg='#FF5733', cursor='hand2', activebackground='#35BD6F')
-     boton_nuevo.grid(row =4, column=0, padx=10, pady=10)
+     boton_nuevo.grid(row =6, column=0, padx=10, pady=10)
 
      boton_guardar = tk.Button(self.frame, text = 'Guardar')
      boton_guardar.config(width=20, font=('Arial', 12, 'bold'), fg='#DAD5D6', bg='#FF5733', cursor='hand2', activebackground='#35BD6F')
-     boton_guardar.grid(row =4, column=1, padx=10, pady=10)
+     boton_guardar.grid(row =6, column=1, padx=10, pady=10)
     
      boton_cancelar = tk.Button(self.frame, text = 'Cancelar')
      boton_cancelar.config(width=20, font=('Arial', 12, 'bold'), fg='#DAD5D6', bg='#FF5733', cursor='hand2', activebackground='#35BD6F')
-     boton_cancelar.grid(row =4, column=2, padx=10, pady=10)
+     boton_cancelar.grid(row =6, column=2, padx=10, pady=10)
   
   def enable_espacios(self):
+    self.checkbox_vehiculo.config(state='normal')
     self.entry_id.config(state='normal')   
-    self.entry_hora.config(state='normal')
+    self.entry_hora_entrada.config(state='normal')
+    self.entry_hora_salida.config(state='normal')
     self.entry_nombre.config(state='normal')
-    
+  def habilitar_placa(self):
+    if self.checkbox_vehiculovar.get():    
+     self.entry_placa.config(state='normal')
+    else:
+      self.entry_placa.config(state='disabled')    
+      
 
     
 
